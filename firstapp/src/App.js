96ae5e717067f1c28c7b4432e65a58c48e95d3a6
NahedIssaoui <issaouinahed96@gmail.com>
2023-06-03 00:52:29 +0100

    first commit

---
+++ firstapp/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import LoginForm from './LoginForm';
+
+
+function App() {
+  return (
+    <div className="App">
+    <div className='sub-App'>
+    <LoginForm />  
+    </div>
+    </div>
+  );
+}
+
+export default App;
